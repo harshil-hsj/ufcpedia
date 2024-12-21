86658f4e637de188d881963df72cfa653bc2ea4e
harshil <harshil.hsj@gmail.com>
2024-12-22 01:48:52 +0530

    added separate dataset for fighters

--- src/components/AddData.js
+++ src/components/AddData.js
@@ -1,12 +1,30 @@
 import './AddData.css'
 import React, { useState } from 'react'
 function AddData(){
-const [fightInfo,setFightInfo] = useState([
+const [fighterInfo,setFighterInfo] = useState([
    
 ]);
+
+const [redFighter,setRedFighter] = useState("");
+
+const [blueFighter,setBlueFighter] = useState("");
+
+
+
 return (
  <div className='AddData'>
       <h1>Add Data for fight</h1>
+      <br/>
+      <input onChange={ (e)=>setRedFighter(e.target.value)}  type='text' placeholder='Enter Fighter in the red corner'/> 
+      <br/>
+      <br/>
+      <input onChange={ (e)=>setBlueFighter(e.target.value)} type='text' placeholder='Enter Fighter in the blue corner'/>
+      <br/>
+      
+      
+      
+
+
  </div>
 )
 }
